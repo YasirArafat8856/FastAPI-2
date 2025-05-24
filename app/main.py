aabf8888a18eb5b9bb60818111e2a3a4a613f111
yasir <yasirarafat8856@gmail.com>
2025-05-24 18:01:44 +0600

    Crud with alembic Orm

--- app/main.py
+++ app/main.py
@@ -1,16 +1,3 @@
-# from fastapi import FastAPI
-
-# app = FastAPI()
-
-# @app.get("/")
-# def read_root():
-#     x = 3
-#     y = 2
-#     if x > y:
-#         return {"1st": "Hello, python World! " + str(x + y)+" "+ str(type(x))}
-#     else:
-#         return {"2nd": "Hello, python World! " + str(x - y)}
-
 
 from fastapi import FastAPI
 from . import models, database, routes
